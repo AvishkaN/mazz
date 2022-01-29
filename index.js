4a5ef01688145340b4da5c16d70425e20a732c88
NIRMITHA <avishkanirmitha@gmail.com>
2022-01-30 00:09:09 +0530

    finished

--- index.js
+++ index.js
@@ -141,7 +141,6 @@ districlist.forEach(btn =>{
 })
 
 
-<<<<<<< HEAD
 const Prev= document.querySelector(".prev")
 const Next= document.querySelector(".next")
 
@@ -188,19 +187,3 @@ Prev.addEventListener("click", function() {
 Next.addEventListener("click", function() {
     slideChange();
   });
-=======
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 3500); // Change image every 2 seconds
-// } 
->>>>>>> parent of bd2d551 (slide on)
